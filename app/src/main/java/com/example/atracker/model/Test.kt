0c474b322e167d730498794e213b360a591aed3e
2taezeat <xorudfl772@naver.com>
2022-05-15 14:56:06 +0900

    feat : LoginActivity.kt, SplashActivity.kt 생성

---
+++ app/src/main/java/com/example/atracker/model/Test.kt
@@ -0,0 +1,4 @@
+package com.example.atracker.model
+
+class Test {
+}
--- app/src/main/java/com/example/atracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/atracker/ui/MainActivity.kt
@@ -1,12 +1,11 @@
-package com.example.atracker
+package com.example.atracker.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.atracker.R
 import com.example.atracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +23,12 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+
+
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+
+
         //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
