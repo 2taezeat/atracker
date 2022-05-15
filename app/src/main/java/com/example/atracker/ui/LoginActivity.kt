08dc9d8556db1df8374e0e4087d0ee15adf1b7ba
2taezeat <xorudfl772@naver.com>
2022-05-15 15:06:15 +0900

    feat : LoginActivity -> SplashActivity

--- app/src/main/java/com/example/atracker/ui/LoginActivity.kt
+++ app/src/main/java/com/example/atracker/ui/LoginActivity.kt
@@ -3,10 +3,20 @@ package com.example.atracker.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.atracker.R
+import com.example.atracker.databinding.ActivityLoginBinding
+import com.example.atracker.databinding.ActivityMainBinding
+import com.example.atracker.databinding.ActivitySplashBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
--- app/src/main/java/com/example/atracker/ui/SplashActivity.kt
+++ app/src/main/java/com/example/atracker/ui/SplashActivity.kt
@@ -1,12 +1,36 @@
 package com.example.atracker.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.atracker.R
+import com.example.atracker.databinding.ActivityMainBinding
+import com.example.atracker.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mHandler = Handler(Looper.getMainLooper())
+        mHandler.postDelayed({
+            startLogin()
+        }, 1000)
+
+
+    }
+
+    fun startLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        ContextCompat.startActivity(this, intent, null)
+        finish()
     }
 }
