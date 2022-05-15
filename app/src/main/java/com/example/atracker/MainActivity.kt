e2409d4d88cf30e3f8dc4c7326801e98162c5b49
2taezeat <xorudfl772@naver.com>
2022-05-15 14:37:39 +0900

    UI : action bar 제거

--- app/src/main/java/com/example/atracker/MainActivity.kt
+++ app/src/main/java/com/example/atracker/MainActivity.kt
@@ -26,11 +26,14 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
 
     }
+
+
+
 
 
 
