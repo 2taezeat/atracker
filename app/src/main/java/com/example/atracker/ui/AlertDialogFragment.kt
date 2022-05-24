b1e4f547ce5959c814efca3f0849a97af47609c9
2taezeat <xorudfl772@naver.com>
2022-05-24 14:23:35 +0900

    ATRK-57, feat : AlertDialogFragment Type 분기

--- app/src/main/java/com/example/atracker/ui/AlertDialogFragment.kt
+++ app/src/main/java/com/example/atracker/ui/AlertDialogFragment.kt
@@ -5,19 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.example.atracker.databinding.FragmentAlertDialogBinding
 import com.example.atracker.databinding.FragmentHomeDisplayBinding
+import com.example.atracker.utils.AlertType
 
-class AlertDialogFragment(alertDialogListener: AlertDialogListener) : DialogFragment() {
+class AlertDialogFragment(private val alertDialogListener: AlertDialogListener, private val alertType : AlertType ) : DialogFragment() {
 
     companion object{
         const val TAG = "AlertDialogFragment"
-        fun instance(alertDialogListener: AlertDialogListener, text: String): AlertDialogFragment {
-            return AlertDialogFragment(alertDialogListener, text)
+        fun instance(alertDialogListener: AlertDialogListener): AlertDialogFragment {
+            return AlertDialogFragment(alertDialogListener)
         }
     }
 
-
-    private var _binding: FragmentHomeDisplayBinding? = null
+    private var _binding: FragmentAlertDialogBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -25,9 +26,28 @@ class AlertDialogFragment(alertDialogListener: AlertDialogListener) : DialogFrag
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-        _binding = FragmentHomeDisplayBinding.inflate(inflater, container, false)
+        _binding = FragmentAlertDialogBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+
+        when (alertType) {
+            AlertType.TYPE1 -> {
+
+            }
+
+            AlertType.TYPE2 -> {
+
+            }
+
+            AlertType.TYPE3 -> {
+
+            }
+            AlertType.TYPE4 -> {
+
+            }
+
+        }
+
 
         return root
     }
---
+++ app/src/main/java/com/example/atracker/utils/AlertType.kt
@@ -0,0 +1,8 @@
+package com.example.atracker.utils
+
+enum class AlertType {
+    TYPE1,
+    TYPE2,
+    TYPE3,
+    TYPE4,
+}
