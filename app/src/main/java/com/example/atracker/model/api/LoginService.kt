138daf48a96c7d806bc8840152029e1f53cd8700
2taezeat <xorudfl772@naver.com>
2022-06-24 16:21:12 +0900

    ATRK-109, UI : LoginActivity UI 개발

---
+++ app/src/main/java/com/example/atracker/model/api/LoginService.kt
@@ -0,0 +1,19 @@
+package com.example.atracker.model.api
+
+import com.example.atracker.model.dto.LoginRequest
+import com.google.gson.JsonObject
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface LoginService {
+
+    @POST("/api/v1/auth/sign")
+    suspend fun loginServiceFun(
+        @Header("Authorization") AccessToken : String,
+        @Body loginRequest : LoginRequest
+
+    ) : retrofit2.Response<JsonObject>
+
+
+}
